00b3b34256299266c8d4bba644b052d739945997
niujp08@qq.com <niujp08@qq.com>
2019-10-23 17:26:20 +0800

    init

--- src/main/scala/example/CubeCalculator.scala
+++ src/main/scala/example/CubeCalculator.scala
@@ -1,8 +1,6 @@
 package example
 
 object CubeCalculator extends App {
-//  val ages = Seq(42, 75, 29, 64)
-//  println(s"The oldest person is ${ages.max}")
   def cube(x: Int) = {
     x * x * x
   }
--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -3,7 +3,7 @@ import org.scalatest.FunSuite
 
 class MainTest extends FunSuite {
   test("CubeCalculator.cube") {
-    assert(CubeCalculator.cube(3) === 27)
+     assert(CubeCalculator.cube(3) === 27)
   }
 }
 
